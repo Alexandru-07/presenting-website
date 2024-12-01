13990fce5a4b43b14936a445bef3ad4c351a261c
Alexandru <alex.alexandru86@yahoo.ro>
2024-12-01 15:32:22 +0200

    Presenting-website Project 1.1

--- src/components/body/body-comp.jsx
+++ src/components/body/body-comp.jsx
@@ -1,8 +1,11 @@
 import "./body-comp.css";
+import BodyExploreComp from "./body-explore-comp/body-explore-comp";
 function BodyComp() {
+
     return (
-        <>
-        </>
+        <div className="body-default">
+            <BodyExploreComp />
+        </div>
     )
 }
 export default BodyComp;
---
+++ src/components/body/body-explore-comp/body-explore-comp.jsx
@@ -0,0 +1,11 @@
+function BodyExploreComp() {
+    return (
+        <div className="body-explore-default" >
+            <div className="body-explore-heading">
+                <h1>EXPLORE</h1>
+            </div>
+        </div>
+    )
+}
+
+export default BodyExploreComp;
